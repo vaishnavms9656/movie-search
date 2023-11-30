94236e74049f2657d55622712b73171c167848d5
vaishnavms9656 <vaishnavms9656@gmail.com>
2023-11-30 22:03:45 +0530

    some changes index

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-
-
-function search(){fetch(`https://www.omdbapi.com/?apikey=ddd5e7b5&t=${key.value}`)
-.then(data => data.json())
-.then(data => {displaymovies(data)})
+function search() {
+  if (key.value == "") {
+    alert("Enter a movie name");
+  } else {
+    fetch(`https://www.omdbapi.com/?apikey=ddd5e7b5&t=${key.value}`)
+      .then((data) => data.json())
+      .then((data) => {
+        displaymovies(data);
+      });
+  }
 }
 
 function displaymovies(data) {
-        htmlData = `
+  htmlData = `
         <div class="text-center ">
         <img src="${data.Poster}" alt="" height="200px">
             <h3 class="text-danger mt-2">${data.Title}</h6>
@@ -15,9 +20,7 @@ function displaymovies(data) {
             <p class="mt-2"><span class="fw-bolder">Actors: </span>${data.Actors}</p>
             <p class="mt-2"><span class="fw-bolder">Imdb: </span>${data.imdbRating}</p>
 
-        </div>`
+        </div>`;
 
-        result.innerHTML = htmlData;
-
-
+  result.innerHTML = htmlData;
 }
